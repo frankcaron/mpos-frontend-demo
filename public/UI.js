341f324c884bf951dc50517d0a46f9a82b87563b
Frank Caron <frank@frankcaron.com>
2019-07-17 10:03:51 -0400

    Adding items to cart works

--- public/UI.js
+++ public/UI.js
@@ -1,6 +1,32 @@
 $( document ).ready(function() {
+
+    //* On Click logic
     $( "div.tile" ).click(function() {
-        console.log("Tile clicked");
+
+        var containerDiv = '<div class="order-line-item">';
+        var orderSelected = '<div class="order-not-selected"></div>';
+        var itemDiv = '<div class="order-item">';
+        var itemContent = '<div class="order-content">' + 'Big Bob M Trio' + '</div>';
+        var priceContent = '<div class="order-content">' + '1x' + '$23.10' + '</div>';
+        var endContainerDiv = '</div>';
+        var orderItem = '<div class="order-item">';
+        var orderItemPrice = '<div class="order-price">' + '$23.10' + '</div>';
+        var end = '</div></div></div>';
+
+        //Clone the order
+        $( "div.tray-order" ).append(
+            containerDiv + 
+            orderSelected + 
+            itemDiv + 
+            itemContent + 
+            priceContent + 
+            endContainerDiv + 
+            orderItem + 
+            orderItemPrice + 
+            end
+        );
+
     });
+
 });
 
