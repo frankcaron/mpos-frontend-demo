5c26ce1736cac4890dab036a81a89154c74dd856
Frank Caron <frank@frankcaron.com>
2019-07-17 07:27:20 -0400

    Tweaked base URL

--- mule.js
+++ mule.js
@@ -1,10 +1,10 @@
 var requestor = require('request');
 var requestlater = require('request-promise')
-const MULE_BASE_URL = 'https://anypoint.mulesoft.com/mocking/api/v1/links/3e041994-280f-4950-b7b1-9ad945bf6a65/'
+const MULE_PRODUCT_BASE_URL = 'http://product-catalogue-system-api.us-e2.cloudhub.io/api/Products'
 
 const getProductsAsync =
     requestlater({
-        uri: MULE_BASE_URL + 'Products',
+        uri: MULE_PRODUCT_BASE_URL,
         json: true
     }).then(function(body) {
         return body;
@@ -12,7 +12,7 @@ const getProductsAsync =
 
 /* Helper function to return all Sites */
 const getProducts = (request, response) => {
-    requestor.get(MULE_BASE_URL + 'Products', { json: true }, (err, res, body) => {
+    requestor.get(MULE_PRODUCT_BASE_URL, { json: true }, (err, res, body) => {
       if (err) { return console.log(err); } 
       response.status(res.statusCode).json(body);
     })
